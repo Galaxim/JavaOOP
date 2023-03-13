92e12824d467bfa93085bec1ef53e191af8c12ac
Kristian <asdf>
2023-03-13 12:18:31 +0100

    junit

---
+++ src/exercises/junit/Calculator.java
@@ -0,0 +1,24 @@
+package exercises.junit;
+
+// You can change any code in this class if you think there is a bug!
+public class Calculator {
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+    public static int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public static int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public static int divide(int a, int b) {
+        return a / b;
+    }
+
+    public static double power(double base, double exponent) {
+        return Math.pow(base,exponent);
+    }
+}
